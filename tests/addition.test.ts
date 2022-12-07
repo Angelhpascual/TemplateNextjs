7b077361eebacf3b37430626c8f4b26c600e0917
angelhpascual <angelhpascual@gmail.com>
2022-12-07 02:20:18 +0100

    added jest testing

---
+++ tests/addition.test.ts
@@ -0,0 +1,7 @@
+import addition from './addition'
+
+describe('addition dummy test', () => {
+  it('adds two numbers', () => {
+    expect(addition(1, 2)).toBe(3)
+  })
+})
---
+++ tests/addition.ts
@@ -0,0 +1,3 @@
+export default function addition(a: number, b: number) {
+  return a + b
+}
